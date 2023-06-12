78c698fcf7fe0485b91b6fca4d34fb0e1da796cb
Umut Berhan <umutberhan94@gmail.com>
2023-06-12 21:52:22 +0300

    api endpoint

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -18,7 +18,7 @@ const CreatePost = () => {
     if(form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('https://dalle-social.onrender.com/api/v1/dalle/', {
+        const response = await fetch('https://umutberhan.netlify.app/api/v1/dalle/', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -45,7 +45,7 @@ const CreatePost = () => {
       setLoading(true);
 
       try {
-        const response = await fetch('https://dalle-social.onrender.com/api/v1/post', {
+        const response = await fetch('https://umutberhan.netlify.app/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
